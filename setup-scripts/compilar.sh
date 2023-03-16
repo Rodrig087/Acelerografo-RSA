743ecf04c149093ea30b02f2991636869f86dd8b
Acel_TestServer <miltonho@hotmail.com>
2023-03-16 22:29:32 +0000

    Se preparo un entorno de instalacion que incluye todos los programas necesarios para desplegar el sistema de registro continuo, asi como un menu y varios sripts de configuracion que facilitan el proceso.

---
+++ setup-scripts/compilar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Compila todos los programas de la carpeta Programas 
+gcc programas/RegistroContinuo_V35.c -o /home/rsa/ejecutables/acelerografo -lbcm2835 -lwiringPi -lm
+gcc programas/ComprobarRegistro_V3.c -o /home/rsa/ejecutables/comprobarregistro
+gcc programas/ExtraerEventoBin_V2.c -o /home/rsa/ejecutables/extraerevento
---
+++ setup-scripts/iniciar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copiar el directorio "Configuracion" a la ruta /home/rsa/
+cp -r configuracion /home/rsa/
+
+# Crear los directorios necesarios
+mkdir -p /home/rsa/ejecutables
+mkdir -p /home/rsa/log-files
+mkdir -p /home/rsa/tmp
+mkdir -p /home/rsa/resultados/eventos-detectados
+mkdir -p /home/rsa/resultados/eventos-extraidos
+mkdir -p /home/rsa/resultados/registro-continuo
+
+# Compila todos los programas de la carpeta Programas 
+gcc /home/rsa/Acelerografo-RSA/programas/RegistroContinuo_V35.c -o /home/rsa/ejecutables/acelerografo -lbcm2835 -lwiringPi -lm
+gcc /home/rsa/Acelerografo-RSA/programas/ComprobarRegistro_V3.c -o /home/rsa/ejecutables/comprobarregistro
+gcc /home/rsa/Acelerografo-RSA/programas/ExtraerEventoBin_V2.c -o /home/rsa/ejecutables/extraerevento
+
+# Copia los task-scripts al directorio /usr/local/bin
+sudo cp task-scripts/comprobar.sh /usr/local/bin/comprobar
+sudo cp task-scripts/informacion.sh /usr/local/bin/informacion 
+sudo cp task-scripts/registrocontinuo.sh /usr/local/bin/registrocontinuo
+
+# Conceder permisos de ejecucion a los task-scripts
+sudo chmod +x /usr/local/bin/comprobar
+sudo chmod +x /usr/local/bin/informacion
+sudo chmod +x /usr/local/bin/registrocontinuo
