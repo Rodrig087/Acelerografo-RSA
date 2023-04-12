10ecf8c0821f0946fe6f33fc5c002747021b5294
Acel_TestServer <miltonho@hotmail.com>
2023-04-12 20:39:30 +0000

    Se trajeron todos los cambios extra que se hicieron en la rama master

--- setup-scripts/compilar.sh
+++ setup-scripts/compilar.sh
@@ -4,3 +4,4 @@
 gcc programas/RegistroContinuo_V35.c -o /home/rsa/ejecutables/acelerografo -lbcm2835 -lwiringPi -lm
 gcc programas/ComprobarRegistro_V3.c -o /home/rsa/ejecutables/comprobarregistro
 gcc programas/ExtraerEventoBin_V2.c -o /home/rsa/ejecutables/extraerevento
+gcc /home/rsa/Acelerografo-RSA/programas/ResetMaster.c -o /home/rsa/ejecutables/resetmaster -lbcm2835 -lwiringPi 
