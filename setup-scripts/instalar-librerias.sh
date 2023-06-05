fc86fc70fe0e8eab1f092e538d2257896f1ead67
Acel_TestServer <miltonho@hotmail.com>
2023-06-05 22:49:51 +0000

    Se corrigieron los scripts iniciar.sh e instalar-librerias.sh

--- setup-scripts/instalar-librerias.sh
+++ setup-scripts/instalar-librerias.sh
@@ -9,8 +9,14 @@ make
 sudo make check
 sudo make install
 
-# Instalacion libreria WirinPi
-sudo apt-get install wiringpi
-
 # Instalacion libreria paho-mqtt
 sudo pip3 install paho-mqtt
+
+# Instalacion libreria Google Drive API
+sudo pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+sudo pip3 install --upgrade oauth2client
+
+# Instalacion libreria WirinPi
+cd /tmp
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
